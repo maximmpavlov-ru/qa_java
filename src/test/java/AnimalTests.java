bc41d71a64abea560f6708b6ba08508bdebb8b56
Maxim Pavlov <maxim.m.pavlov@gmail.com>
2025-03-28 11:17:10 +0600

    First commit

--- src/test/java/AnimalTests.java
+++ src/test/java/AnimalTests.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import com.example.Animal;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class AnimalTests {
+
+    @Test
+    public void checkGetFamilyMethod() {
+        String expectedMessage = "Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи";
+        Assert.assertEquals("Неправильный список семейств", expectedMessage, new Animal().getFamily());
+    }
+
+    @Test
+    public void checkGetFoodMethodWithException() {
+        try {
+            new Animal().getFood("Dial-up модем");
+        } catch (Exception exception) {
+            System.out.println("Неизвестный вид животного, используйте значение Травоядное или Хищник");
+        }
+    }
 }
--- src/test/java/CatTests.java
+++ src/test/java/CatTests.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.List;
+
+
+@RunWith(MockitoJUnitRunner.class)
 public class CatTests {
+    @Mock
+    Feline feline;
+
+    @Test
+    public void testGetSoundMethod() {
+        String normalSound = "Мяу";
+
+        Assert.assertEquals("Этот кот издает неправильные звуки", normalSound, new Cat(feline).getSound());
+    }
+
+    @Test
+    public void testGetFoodMethod() throws Exception {
+        List<String> normalFood = List.of("Мясо", "Рыба");
+        Mockito.when(feline.eatMeat()).thenReturn(normalFood);
+
+        Assert.assertEquals("Наш кот такое есть не любит", normalFood, new Cat(feline).getFood());
+    }
 }
